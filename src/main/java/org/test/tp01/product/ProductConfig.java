ad06234d5d4a1dfc62f66e7975e77b8988ad1fcb
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-24 10:11:11 +0100

    add ProductConfig

---
+++ src/main/java/org/test/tp01/product/ProductConfig.java
@@ -0,0 +1,26 @@
+package org.test.tp01.product;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductConfig {
+
+    private final ProductRepository productRepository;
+
+    public ProductConfig(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Bean
+    CommandLineRunner init_product() {
+        return args -> {
+          productRepository.save(new Product("R001","Iphone13",1300,50));
+          productRepository.save(new Product("R002","Sumsung Note 20",1500,100));
+          productRepository.save(new Product("R003","TV Sony",13000,5));
+          productRepository.save(new Product("R004","Iphone 12",700,100));
+          productRepository.save(new Product("R005","PS5",400,500));
+        };
+    }
+}
