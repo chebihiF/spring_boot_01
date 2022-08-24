cbd20733e60bde2e3e9ba7e12f74a3757ccdea39
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-24 09:25:08 +0100

    Initial commit

---
+++ src/test/java/org/test/tp01/Tp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.tp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
