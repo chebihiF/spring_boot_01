e6ffe43b3ff00699bde6842c04182db254c80db2
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-25 11:30:31 +0100

    add ApplicationUserDaoRepoMySql

--- src/main/java/org/test/tp01/auth/ApplicationUserDaoRepoMySql.java
+++ src/main/java/org/test/tp01/auth/ApplicationUserDaoRepoMySql.java
@@ -1,8 +1,16 @@
 package org.test.tp01.auth;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Repository;
+import org.test.tp01.user.Authority;
 import org.test.tp01.user.AuthorityRepository;
+import org.test.tp01.user.User;
 import org.test.tp01.user.UserRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
 public class ApplicationUserDaoRepoMySql implements ApplicationUserDao{
@@ -18,8 +26,26 @@ public class ApplicationUserDaoRepoMySql implements ApplicationUserDao{
     @Override
     public ApplicationUserDetail getUserByUsername(String username) {
 
+        User user = userRepository.findUserByUsername(username);
+        List<Authority> authorities = authorityRepository.findAuthoritiesByUsersContains(user);
+        Set<SimpleGrantedAuthority> grantedAuthorities = authorities
+                .stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toSet());
 
+        /*Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+        for(Authority authority : authorities){
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }*/
 
-        return null;
+        return new ApplicationUserDetail(
+                user.getPassword(),
+                user.getUsername(),
+                grantedAuthorities,
+                user.isAccountNonExpired(),
+                user.isAccountNonLocked(),
+                user.isCredentialsNonExpired(),
+                user.isEnabled()
+        );
     }
 }
--- src/main/java/org/test/tp01/auth/ApplicationUserDetail.java
+++ src/main/java/org/test/tp01/auth/ApplicationUserDetail.java
@@ -1,6 +1,7 @@
 package org.test.tp01.auth;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -9,13 +10,13 @@ import java.util.Set;
 public class ApplicationUserDetail implements UserDetails {
     private final String password;
     private final String username;
-    private final Set<GrantedAuthority> authorities;
+    private final Set<SimpleGrantedAuthority> authorities;
     private final boolean accountNonExpired;
     private final boolean accountNonLocked;
     private final boolean credentialsNonExpired;
     private final boolean enabled;
 
-    public ApplicationUserDetail(String password, String username, Set<GrantedAuthority> authorities, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, boolean enabled) {
+    public ApplicationUserDetail(String password, String username, Set<SimpleGrantedAuthority> authorities, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, boolean enabled) {
         this.password = password;
         this.username = username;
         this.authorities = authorities;
