c18c464703d47c8cbccce53aeea7bbc3e5a63f45
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-25 10:31:02 +0100

    package user

---
+++ src/main/java/org/test/tp01/user/Authority.java
@@ -0,0 +1,23 @@
+package org.test.tp01.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Authority {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String name ;
+    @ManyToMany
+    @JoinTable(
+            name = "role",
+            joinColumns = @JoinColumn(name = "id_auth"),
+            inverseJoinColumns = @JoinColumn(name = "id_user")
+    )
+    private List<User> users ;
+}
